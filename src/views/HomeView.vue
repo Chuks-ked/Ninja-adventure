614a78bb6f9056ffa0980f15a888f1d65cfdc0fa
Chuks <ikedichukwubuikem@gmail.com>
2023-03-15 09:07:56 +0100

    Octo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur animi voluptatibus aliquid error fugit nulla libero, sint, eligendi, ipsa in consectetur! Odio beatae repudiandae omnis dolor illum veniam nemo. Unde laudantium debitis fugiat molestias maiores deleniti! Corrupti harum architecto dolores! Aliquam ullam vel ducimus blanditiis odio voluptate sed beatae ipsam dolore possimus doloribus dolorem at suscipit pariatur, adipisci sit totam placeat tenetur nostrum soluta sequi tempora non ad. Voluptatibus, nam! Fugit reprehenderit ea nobis quia voluptatem tempora odio consectetur? Ex, exercitationem fugit vitae optio harum enim omnis. Labore fuga exercitationem autem nulla facilis eligendi in, ut ex deserunt enim ad.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur animi voluptatibus aliquid error fugit nulla libero, sint, eligendi, ipsa in consectetur! Odio beatae repudiandae omnis dolor illum veniam nemo. Unde laudantium debitis fugiat molestias maiores deleniti! Corrupti harum architecto dolores! Aliquam ullam vel ducimus blanditiis odio voluptate sed beatae ipsam dolore possimus doloribus dolorem at suscipit pariatur, adipisci sit totam placeat tenetur nostrum soluta sequi tempora non ad. Voluptatibus, nam! Fugit reprehenderit ea nobis quia voluptatem tempora odio consectetur? Ex, exercitationem fugit vitae optio harum enim omnis. Labore fuga exercitationem autem nulla facilis eligendi in, ut ex deserunt enim ad.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
